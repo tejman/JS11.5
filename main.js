48c8864295c9aa59897511ad74d612b7a63fa38c
tejman <rudrarajut@gmail.com>
2014-01-27 17:19:44 -0700

    final commit

--- main.js
+++ main.js
@@ -1,21 +1,58 @@
 $(function() {
+  var clock = function() {
+    $(".container").empty();
 
-  var outerShell = $("<div class='outer-shell'></div>");
-  var innerShell = $("<div class='inner-shell'></div>");
-  var leftLabels = $("<div class='left-labels'><li>PM</li><li>AUTO</li></div>")
-  var clockScreen = $("<div class='clock-screen'></div>");
-  var ampm = $("<div class='ampm'><li></li><li>&bull;</li></div>");
-  var clockText = $("<div class='clock-text'>12:17</div>")
-  var amLabel = $("<span class='am-label'><span class='freq'></span></span>")
-  var pmLabel = $("<span class='pm-label'><span class='freq'></span></span>")
+    var myTime = new Date();
+    
+    if (myTime.getMinutes()<10) {
+      var min = "0"+myTime.getMinutes().toString();
+    }
+    else {
+      var min = myTime.getMinutes();
+    };
 
-	
-  outerShell.appendTo(".container");
-  innerShell.appendTo(outerShell);
-  leftLabels.appendTo(innerShell);
-  clockScreen.appendTo(innerShell);
-  ampm.appendTo(clockScreen);
-  clockText.appendTo(clockScreen);
-  amLabel.appendTo(innerShell);
-  pmLabel.appendTo(innerShell);
+    if (myTime.getHours()>12) {
+      var hour = (myTime.getHours()-12);
+      var pm = true;
+    }
+    else {
+      var hour = myTime.getHours();
+      var pm = false;
+    };
+
+    var currentTime = hour+":"+min;
+
+
+    var outerShell = $("<div class='outer-shell'></div>");
+    var innerShell = $("<div class='inner-shell'></div>");
+    var leftLabels = $("<div class='left-labels'><li>PM</li><li>AUTO</li></div>")
+    var clockScreen = $("<div class='clock-screen'></div>");
+    var ampm = $("<div class='ampm'><li>{0}</li><li></li></div>".supplant([pm ? "&bull;" : ""]));
+    var clockText = $("<div class='clock-text'>{0}</div>".supplant([currentTime]));
+    var amLabel = $("<div class='am-label'>AM<pre class='freq'></pre></div>")
+    var pmLabel = $("<div class='pm-label'>FM<pre class='freq'></pre></div>")
+
+    var amFreq = "   53    60    70    90    110    140    170"
+    var pmFreq = "   88    92    96    102    106    108"
+
+  	
+    outerShell.appendTo(".container");
+    innerShell.appendTo(outerShell);
+    leftLabels.appendTo(innerShell);
+    clockScreen.appendTo(innerShell);
+    ampm.appendTo(clockScreen);
+    clockText.appendTo(clockScreen);
+    amLabel.appendTo(innerShell);
+    pmLabel.appendTo(innerShell);
+    amLabel.find(".freq").text(amFreq);
+    pmLabel.find(".freq").text(pmFreq);
+
+    // console.log(myTime.getSeconds());
+
+  };
+
+  clock();
+  setInterval(clock, 5000);
+
+  
 });
